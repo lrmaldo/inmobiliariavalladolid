0fc33fb66b4254c72ffc96068d3c800687d8b9c8
Leonardo Rodrigo Maldonado López <lrmaldo@gmail.com>
2017-06-16 19:15:51 -0500

    Add files via upload

---
+++ common/config/main-local.php
@@ -0,0 +1,30 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=inmobiliaria',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport'=> [
+                'class' =>  'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username'=>'leodeveloper93@gmail.com',
+                'password'=>'leonardito',
+                'port'=>'587',
+                'encryption' =>'tls',
+                
+                
+            ]
+        ],
+    ],
+];
