8625a2fb030913cbae8a3f3462eb07457e8b4300
Carlos Soberanis Cupul <carlos.soberanisc@outlook.com>
2017-06-16 20:24:18 -0500

    Nuevas correcciones

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=inmobiliaria',
             'username' => 'root',
-            'password' => '',
+            'password' => 'mysql',
             'charset' => 'utf8',
         ],
         'mailer' => [
